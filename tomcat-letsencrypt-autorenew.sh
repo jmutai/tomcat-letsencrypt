e6bc6ee77314e9cc17bf8543727be38f28bf7397
Josphat Mutai <mutai@jmutai.com>
2018-03-07 15:15:19 +0300

    Update tomcat-letsencrypt-autorenew.sh

--- tomcat-letsencrypt-autorenew.sh
+++ tomcat-letsencrypt-autorenew.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 DOMAIN=""
 TOMCAT_KEY_PASS=""
 CERTBBOT_BIN="/usr/local/bin/certbot-auto"
@@ -8,15 +9,14 @@ EMAIL_NOTIFICATION="email_address"
 
 install_certbot () {
     if [[ ! -f /usr/local/bin/certbot-auto ]]; then
-    wget https://dl.eff.org/certbot-auto -P /usr/local/bin
-    chmod a+x $CERTBOT_BIN
-  fi
+        wget https://dl.eff.org/certbot-auto -P /usr/local/bin
+        chmod a+x $CERTBOT_BIN
+    fi
 }
 
 # Attempt cert renewal:
-
 renew_ssl () {
-    /usr/local/bin/certbot-auto renew  > /tmp/crt.txt
+    ${CERTBOT_BIN} renew  > /tmp/crt.txt
     cat /tmp/crt.txt | grep "No renewals were attempted"
     if [[ $? -eq "0" ]]; then
         echo "Cert not yet due for renewal"
@@ -34,35 +34,35 @@ renew_ssl () {
        -in /etc/letsencrypt/live/$DOMAIN/fullchain.pem \
        -inkey /etc/letsencrypt/live/$DOMAIN/privkey.pem \
        -name tomcat
-       
+ fi
+ }
+
       # Convert that PKCS12 to a JKS
     rm -f /etc/ssl/${DOMAIN}.jks 2>/dev/null
     keytool -importkeystore -deststorepass $TOMCAT_KEY_PASS -destkeypass $TOMCAT_KEY_PASS \
       -destkeystore /etc/ssl/${DOMAIN}.jks -srckeystore /tmp/${DOMAIN}_fullchain_and_key.p12  \
       -srcstoretype PKCS12 -srcstorepass $TOMCAT_KEY_PASS \
       -alias tomcat
- }
- 
+
 # Send email notification on completion
 send_email_notification () {
-  if [[ $? -eq "0" ]]; then
-   echo " Retarting tomcat server"
-  systemctl restart tomcat
-  if [[ $? -eq "0" ]]; then
-    echo "" > /tmp/success
-    echo "Letsencrypt ssl certificate for $DOMAIN successfully renewed by cron job." >> /tmp/success
-    echo "" >> /tmp/success
-    echo "Tomcat successfully restarted after renewal" >> /tmp/success
-    mail -s "$DOMAIN Letsencrypt renewal" support-notify@angani.co < /tmp/success
-  else
-    echo "" > /tmp/failure
-    echo "Letsencrypt ssl certificate for $DOMAIN renewal by cron job failed." >> /tmp/failure
-    echo "" >> /tmp/failure
-    echo "Try again manually.." >> /tmp/failure
-    mail -s "$DOMAIN Letsencrypt renewal" $EMAIL_NOTIFICATION < /tmp/failure
-  fi
-fi
-fi
+    if [[ $? -eq "0" ]]; then
+        echo " Retarting tomcat server"
+        systemctl restart tomcat
+        if [[ $? -eq "0" ]]; then
+            echo "" > /tmp/success
+            echo "Letsencrypt ssl certificate for $DOMAIN successfully renewed by cron job." >> /tmp/success
+            echo "" >> /tmp/success
+            echo "Tomcat successfully restarted after renewal" >> /tmp/success
+            mail -s "$DOMAIN Letsencrypt renewal" support-notify@angani.co < /tmp/success
+        else
+            echo "" > /tmp/failure
+            echo "Letsencrypt ssl certificate for $DOMAIN renewal by cron job failed." >> /tmp/failure
+            echo "" >> /tmp/failure
+            echo "Try again manually.." >> /tmp/failure
+            mail -s "$DOMAIN Letsencrypt renewal" $EMAIL_NOTIFICATION < /tmp/failure
+        fi
+    fi
 }
 
 # Main
